ef421f4ba949ef461c942b2e5bc6879634341eb5
Muhammed-Ayad <mohamedayadgithub@gmail.com>
2024-03-24 14:18:15 +0200

    add DirectionSnake

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,44 @@
 import 'dart:async';
+import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+enum DirectionSnake { up, down, left, right }
+
+class Snake {
+  final List<int> length;
+
+  Snake(this.length);
+
+  Snake copyWith({
+    List<int>? length,
+  }) {
+    return Snake(
+      length ?? this.length,
+    );
+  }
+
+  @override
+  String toString() => 'Snake($length)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Snake && listEquals(other.length, length);
+  }
+
+  @override
+  int get hashCode => length.hashCode;
+}
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +53,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -32,6 +63,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int snake = 0;
   int moveTo = 1;
   List<int> snakes = [];
+  Map<Snake, DirectionSnake> moveSnake = {};
+  DirectionSnake direction = DirectionSnake.right;
 
   @override
   void initState() {
@@ -39,7 +72,37 @@ class _MyHomePageState extends State<MyHomePage> {
     Timer.periodic(const Duration(milliseconds: 200), (_) {
       setState(() {
         snake = snake == 400 ? 0 : snake + moveTo;
-        snakes = [snake + moveTo, snake + moveTo + 1, snake + moveTo + 2];
+        moveSnake = {
+          Snake([snake, snake + 1, snake + 2]): direction,
+        };
+        moveSnake.forEach((key, value) {
+          switch (value) {
+            case DirectionSnake.up:
+              moveTo = -20;
+              snakes = [
+                snake + moveTo,
+                snake + moveTo + 20,
+                snake + moveTo + 40
+              ];
+              break;
+            case DirectionSnake.down:
+              moveTo = 20;
+              snakes = [
+                snake + moveTo,
+                snake + moveTo + 20,
+                snake + moveTo + 40
+              ];
+              break;
+            case DirectionSnake.left:
+              moveTo = -1;
+              snakes = [snake + moveTo, snake + moveTo + 1, snake + moveTo + 2];
+              break;
+            case DirectionSnake.right:
+              moveTo = 1;
+              snakes = [snake + moveTo, snake + moveTo + 1, snake + moveTo + 2];
+              break;
+          }
+        });
       });
     });
   }
@@ -48,7 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.secondary,
         title: const Text('Snake Game'),
         centerTitle: true,
       ),
@@ -65,18 +128,16 @@ class _MyHomePageState extends State<MyHomePage> {
                   color: Colors.grey,
                 ),
               ),
-              child: GridView(
+              child: GridView.builder(
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 20,
                 ),
-                children: List.generate(
-                  400,
-                  (index) => Container(
-                    margin: const EdgeInsets.all(1.0),
-                    width: 15.0,
-                    height: 15.0,
-                    color: snakes.contains(index) ? Colors.red : Colors.white,
-                  ),
+                itemCount: 400,
+                itemBuilder: (context, index) => Container(
+                  margin: const EdgeInsets.all(1.0),
+                  width: 15.0,
+                  height: 15.0,
+                  color: snakes.contains(index) ? Colors.red : Colors.white,
                 ),
               ),
             ),
@@ -84,40 +145,24 @@ class _MyHomePageState extends State<MyHomePage> {
           Column(
             children: [
               CustomIconButton(
-                onPressed: () {
-                  setState(() {
-                    moveTo = -20;
-                  });
-                },
+                onPressed: () => _setDirection(DirectionSnake.up),
                 icon: Icons.arrow_upward_sharp,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   CustomIconButton(
-                    onPressed: () {
-                      setState(() {
-                        moveTo = -1;
-                      });
-                    },
+                    onPressed: () => _setDirection(DirectionSnake.left),
                     icon: Icons.arrow_back,
                   ),
                   CustomIconButton(
-                    onPressed: () {
-                      setState(() {
-                        moveTo = 1;
-                      });
-                    },
+                    onPressed: () => _setDirection(DirectionSnake.right),
                     icon: Icons.arrow_forward_outlined,
                   ),
                 ],
               ),
               CustomIconButton(
-                onPressed: () {
-                  setState(() {
-                    moveTo = 20;
-                  });
-                },
+                onPressed: () => _setDirection(DirectionSnake.down),
                 icon: Icons.arrow_downward_outlined,
               ),
             ],
@@ -126,17 +171,26 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  void _setDirection(DirectionSnake newDirection) {
+    setState(() {
+      direction = newDirection;
+    });
+    log(direction.toString());
+    log(moveSnake.keys.toString());
+  }
 }
 
-// create custom icon button
 class CustomIconButton extends StatelessWidget {
   const CustomIconButton({
     super.key,
     required this.onPressed,
     required this.icon,
   });
+
   final VoidCallback onPressed;
   final IconData icon;
+
   @override
   Widget build(BuildContext context) {
     return IconButton(
